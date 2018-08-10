324fdcf7cfd7de7f0b9d2ec38f1071af130b3734
kukiron <kafiluddin.kiron@gmail.com>
2018-08-10 17:00:29 +0600

    Create utility functions for the app

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+export const formatJsonData = (firstArr, secArr) => {
+  let data = [];
+  for (let item of secArr) {
+    firstArr.forEach(
+      elem => elem.id === item.id && data.push({ ...elem, ...item })
+    );
+  }
+
+  return data;
+};
+
+export const addAttribute = (item, arr) => {
+  const newArr = arr.map(elem => {
+    if (elem.id === item.id) {
+      const attributes = elem.attributes.concat(item.attribute);
+      return Object.assign({}, elem, { attributes });
+    } else return elem;
+  });
+
+  return newArr;
+};
