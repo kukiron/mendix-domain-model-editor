b050b6a48f78e8dab1d3b04047301c10cd2e1ed9
kukiron <kafiluddin.kiron@gmail.com>
2018-08-10 16:57:34 +0600

    Render attribute names & types

--- src/components/entitycanvas.jsx
+++ src/components/entitycanvas.jsx
@@ -8,21 +8,34 @@ const entityBaseStyle = {
   backgroundColor: '#fafafa',
   border: '1px solid cornflowerblue',
   borderRadius: 4,
-  padding: 20
+  padding: 20,
+  cursor: 'pointer'
 };
 
-export const EntityCanvas = observer(({ entityStore }) => (
+export const EntityCanvas = observer(({ entityStore, onBlur, onFocus }) => (
   <div>
-    {entityStore.entities.map(entity => (
-      <Entity key={entity.id} entity={entity} />
+    {entityStore.entities.map((entity, i) => (
+      <Entity key={i} entity={entity} onBlur={onBlur} onFocus={onFocus} />
     ))}
   </div>
 ));
 
-const Entity = observer(({ entity }) => (
+const Entity = observer(({ entity, onBlur, onFocus }) => (
   <Draggable defaultPosition={{ x: entity.x, y: entity.y }}>
-    <div className="draggable" style={Object.assign({}, entityBaseStyle)}>
+    <div
+      className="draggable"
+      style={Object.assign({}, entityBaseStyle)}
+      onFocus={() => onFocus(entity)}
+      onBlur={onBlur} tabIndex={0}
+    >
       {entity.name}
+      {entity.attributes &&
+        entity.attributes.length > 0 &&
+        entity.attributes.map((attr, i) => (
+          <div key={i}>
+            <b>{attr.name}</b>: <i>{attr.type}</i>
+          </div>
+        ))}
     </div>
   </Draggable>
 ));
