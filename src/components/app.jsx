277de1bf57e555e8c0f732ebca5b8b4a1f4223f6
kukiron <kafiluddin.kiron@gmail.com>
2018-08-10 16:56:22 +0600

    Configure events for modal & rendered entities

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,69 @@
+import 'babel-polyfill';
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+
+import AttributeModal from './attributeModal';
 import { EntityCanvas } from './entitycanvas';
 
 @observer
 export class App extends Component {
+  state = { active: false, modalIsOpen: false, entity: null };
+
+  handleBlur = () => {
+    this.setState({ active: false });
+  };
+
+  handleFocus = entity => {
+    this.setState({ active: true, entity });
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleOnAfterOpenModal = () => {
+    return this.state.entity;
+  };
+
   render() {
     return (
       <div>
         <h1>Domain Model Editor</h1>
+        <h4>Select an entity to add new attribute</h4>
         <button onClick={this.onAddEntity}>Add Entity</button>
-        <EntityCanvas entityStore={this.props.entityStore} />
+        <button
+          onMouseDown={this.openModal}
+          disabled={!this.state.active}
+        >
+          Add New Attribute
+        </button>
+        <AttributeModal
+          {...this.props}
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.handleOnAfterOpenModal}
+          closeModal={this.closeModal}
+        />
+        <EntityCanvas
+          entityStore={this.props.entityStore}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
       </div>
     );
   }
 
   onAddEntity = () => {
-    const entityName = prompt("Name of the new entity", "");
-    if (entityName) {
-      this.props.entityStore.addEntity(entityName, Math.random() * 1000, Math.random() * 1000);
+    const entityName = prompt('Name of the new entity', '');
+    if (window && entityName) {
+      this.props.entityStore.addEntity(
+        entityName,
+        Math.random() * innerWidth,
+        Math.random() * innerHeight
+      );
     }
-  }
-};
+  };
+}
