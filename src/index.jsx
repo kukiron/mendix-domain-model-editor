e72b1c25e9cfc8c3917de1255d8d12b6870ab7e2
kukiron <kafiluddin.kiron@gmail.com>
2018-09-04 13:33:49 +0600

    Implement hot reloading

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,44 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { App } from './components/app';
-import { EntityStore } from './stores/entitystore';
-import { useStrict } from 'mobx';
-import { formatJsonData } from './utils';
+import "babel-polyfill"
+import React from "react"
+import { render } from "react-dom"
+import { AppContainer } from "react-hot-loader"
+import { App } from "./components/app"
+import { EntityStore } from "./stores/entitystore"
+import { useStrict } from "mobx"
+import { formatJsonData } from "./utils"
 
-useStrict(true);
+useStrict(true)
 
-const entityStore = new EntityStore();
+const entityStore = new EntityStore()
 const loadEntityStoreData = async () => {
-  const coordsResponse = await fetch('http://localhost:3000/static/coords.json');
-  const coords = await coordsResponse.json();
-  const entitiesResponse = await fetch('http://localhost:3000/static/entities.json');
-  const entities = await entitiesResponse.json();
+  const coordsResponse = await fetch("http://localhost:3000/static/coords.json")
+  const coords = await coordsResponse.json()
+  const entitiesResponse = await fetch(
+    "http://localhost:3000/static/entities.json"
+  )
+  const entities = await entitiesResponse.json()
 
-  const DEMO_DATA = formatJsonData(coords, entities);
-  entityStore.loadJson(DEMO_DATA);
-};
-loadEntityStoreData();
+  const DEMO_DATA = formatJsonData(coords, entities)
+  entityStore.loadJson(DEMO_DATA)
+}
+loadEntityStoreData()
 
 render(
   <AppContainer>
     <App entityStore={entityStore} />
   </AppContainer>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 if (module.hot) {
-  module.hot.accept('./components/app', () => {
-    const NextApp = require('./components/app').App;
+  module.hot.accept("./components/app", () => {
+    const NextApp = require("./components/app").App
 
     render(
       <AppContainer>
-        <App entityStore={entityStore} />
+        <NextApp entityStore={entityStore} />
       </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+      document.getElementById("root")
+    )
+  })
 }
